612327d1e1535e4ee11ea8395528302099969c77
Michał Węglarz <michal.weglarz@protonmail.com>
2019-05-12 15:35:40 +0200

    Added script to rotate reviews

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const navbar = document.querySelector("header").children[0];
-
 let navbarHeight = navbar.getBoundingClientRect().height;
 
 window.onscroll = () => {
@@ -12,3 +11,35 @@ window.onscroll = () => {
     navbar.classList.remove("scroll");
   }
 };
+
+const reviewButtons = document.querySelectorAll(".reviewButton");
+let allReviews = Array.from(document.querySelectorAll(".review"));
+allReviews.forEach((item, idx) => {
+  if (idx !== 0) item.style.display = "none";
+});
+
+let changeReviews = (pressType, indexToShow) => {
+  allReviews.forEach((item, index) => {
+    if (index === indexToShow) {
+      item.style.display = "block";
+    } else item.style.display = "none";
+  });
+};
+
+let changeIndexToShow = (pressType, indexToShow) => {
+  if (pressType === "nextReview") {
+    return (indexToShow + 1) % allReviews.length;
+  }
+  if (pressType === "prevReview") {
+    return (indexToShow + allReviews.length - 1) % allReviews.length;
+  }
+};
+
+let indexToShow = 0;
+reviewButtons.forEach(
+  item =>
+    (item.onclick = () => {
+      indexToShow = changeIndexToShow(item.id, indexToShow);
+      changeReviews(item.id, indexToShow);
+    })
+);
